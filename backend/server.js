220af4b5c3c0ae6d90782fd66922260b60e63886
LearnerShym1947 <learner.shyam@gmail.com>
2026-06-15 08:40:29 +0530

    add vercel config

--- backend/server.js
+++ backend/server.js
@@ -4,316 +4,177 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-const Enquiry =
-require("./models/Enquiry");
+const Enquiry = require("./models/Enquiry");
 
-const app =
-express();
+const app = express();
 
 app.use(cors());
-
-app.use(
-express.json()
-);
+app.use(express.json());
 
 mongoose
-.connect(
-process.env.MONGO_URI
-)
-.then(()=>{
-
-console.log(
-"MongoDB Connected"
-);
-
+.connect(process.env.MONGO_URI)
+.then(() => {
+console.log("MongoDB Connected");
 })
-.catch((err)=>{
+.catch((err) => {
+console.log(err);
+});
 
-console.log(
-err
-);
 
+// HOME ROUTE
+app.get("/", (req,res)=>{
+res.send("Backend Running");
 });
 
 
 // CREATE
-
-app.post(
-
-"/api/enquiry",
-
-async(req,res)=>{
+app.post("/api/enquiry", async (req,res)=>{
 
 try{
 
-const {
+const { name,email,phone } = req.body;
 
-name,
-email,
-phone
+if(!name || !email || !phone){
 
-}
-
-=
-
-req.body;
-
-if(
-
-!name ||
-
-!email ||
-
-!phone
-
-){
-
-return res
-.status(400)
-.json({
-
+return res.status(400).json({
 success:false,
-
-message:
-
-"All fields required"
-
+message:"All fields required"
 });
 
 }
 
 const exists =
-
 await Enquiry.findOne({
 
 $or:[
-
 {email},
-
 {phone}
-
 ]
 
 });
 
-if(
+if(exists){
 
-exists
-
-){
-
-return res
-.status(400)
-.json({
-
+return res.status(400).json({
 success:false,
-
-message:
-
-"Email or Phone already used"
-
+message:"Email or Phone already used"
 });
 
 }
 
 await Enquiry.create({
-
 name,
 email,
 phone
-
 });
 
 res.json({
-
 success:true,
-
-message:
-
-"Registration Successful"
-
+message:"Registration Successful"
 });
 
 }
 
 catch(error){
 
-console.log(
-error
-);
+console.log(error);
 
-res
-.status(500)
-
-.json({
-
+res.status(500).json({
 success:false,
-
-message:
-
-error.message
-
+message:error.message
 });
 
 }
 
-}
-
-);
+});
 
 
 // READ
-
-app.get(
-
-"/api/students",
-
-async(req,res)=>{
+app.get("/api/students", async(req,res)=>{
 
 try{
 
-const students =
+const data =
+await Enquiry.find().sort({
+createdAt:-1
+});
 
-await Enquiry.find();
-
-res.json(
-
-students
-
-);
+res.json(data);
 
 }
 
-catch{
+catch(error){
 
-res
-.status(500)
-
-.json({
-
-message:
-
-"Error"
-
+res.status(500).json({
+success:false,
+message:error.message
 });
 
 }
 
-}
-
-);
+});
 
 
 // UPDATE
-
-app.put(
-
-"/api/students/:id",
-
-async(req,res)=>{
+app.put("/api/students/:id", async(req,res)=>{
 
 try{
 
 const updated =
-
 await Enquiry.findByIdAndUpdate(
-
 req.params.id,
-
 req.body,
+{ new:true }
+);
 
-{
-
-new:true
+res.json(updated);
 
 }
 
-);
+catch{
 
-res.json(
-
-updated
-
-);
-
-}
-
-catch(error){
-
-res
-.status(500)
-
-.json({
-
+res.status(500).json({
 success:false,
-
-message:
-
-"Update Failed"
-
+message:"Update Failed"
 });
 
 }
 
-}
-
-);
+});
 
 
 // DELETE
-
-app.delete(
-
-"/api/students/:id",
-
-async(req,res)=>{
+app.delete("/api/students/:id", async(req,res)=>{
 
 try{
 
 await Enquiry.findByIdAndDelete(
-
 req.params.id
-
 );
 
 res.json({
-
 success:true
-
 });
 
 }
 
 catch{
 
-res
-.status(500)
-
-.json({
-
+res.status(500).json({
 success:false
-
 });
 
 }
 
-}
+});
 
-);
+const PORT =
+process.env.PORT || 5000;
 
-
-app.listen(
-
-5000,
-
-()=>{
+app.listen(PORT,()=>{
 
 console.log(
-
-"Server Running on Port 5000"
-
+`Server Running ${PORT}`
 );
 
-}
-
-);
+});
